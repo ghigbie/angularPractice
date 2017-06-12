5e73f005366c74cc87a1a6d0296585b587482dcb
George Higbie <gehigbie@gmail.com>
2017-06-12 21:42:52 +0000

    Add root route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require("express"),
 const port = process.env.PORT,
         ip = process.env.IP;
         
+app.get("/", (req, res)=>{
+    res.send("This is the landing page!");
+});
+        
 app.listen(port, ip, () => {
     console.log(`Express server is listening on port ${port}`);
 });
