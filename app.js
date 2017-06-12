d34c8d2662a0fb2d655f72cd1c9076f70d9e9d94
George Higbie <gehigbie@gmail.com>
2017-06-12 22:02:43 +0000

    Install angular animate

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const express = require("express"),
 const port = process.env.PORT,
         ip = process.env.IP;
         
-app.get("/", (req, res)=>{
+app.get("/", (req, res) =>{
+    console.log("You hit the landing page!");
     res.send("This is the landing page!");
 });
+
+app.get("/data", (req, res) => {
+    res.render("databinding.html");
+});
+
         
 app.listen(port, ip, () => {
     console.log(`Express server is listening on port ${port}`);
