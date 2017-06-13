87d99702a2c66bee0b5aa18deddaa97269ce17e4
George Higbie <gehigbie@gmail.com>
2017-06-13 20:18:26 +0000

    Add controller to separate dir

---
+++ app/controllers/customersController.js
@@ -0,0 +1,18 @@
+function CustomersController($scope){
+    $scope.sortBy = 'name' //provides a default sort
+    $scope.reverse = 'false' //provides a ascending default
+                
+    $scope.customers = [
+        {name: 'John', city: 'Chandler', joined: '2000-12-02', orderTotal: 300}, 
+        {name: 'Mary', city: 'Englewood', joined: '2010-10-12', orderTotal: 400}, 
+        {name: 'Huck', city: 'Yuma', joined: '2013-09-09', orderTotal: 500}, 
+        {name: 'Jackie', city: 'Chandler', joined: '2000-12-02', orderTotal: 300}, 
+        {name: 'Monika', city: 'Englewood', joined: '2010-10-12', orderTotal: 200},
+        {name: 'Caleb', city: 'Neches', joined: '1995-08-02', orderTotal: 300}
+    ];
+    
+    $scope.doSort = function(propName) {
+        $scope.sortBy = propName;
+        $scope.reverse = !$scope.reverse;
+    };
+}
